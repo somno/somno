a921a9682ce099750343061f2963342f34ed4037
David Miller <david@deadpansincerity.com>
2018-09-27 16:35:39 +0100

    Re-point home from welcome controller to today list

--- somno/assets/js/somno/routes.js
+++ somno/assets/js/somno/routes.js
@@ -4,10 +4,5 @@ app.controller('WelcomeCtrl', function(){});
 app.config(
     ['$routeProvider',
      function($routeProvider){
-//	     $routeProvider.when('/',  {redirectTo: '/list'})
-
-         $routeProvider.when('/',  {
-             controller: 'WelcomeCtrl',
-             templateUrl: '/templates/welcome.html'
-           })
+	 $routeProvider.when('/',  {redirectTo: '/list'})
      }]);
