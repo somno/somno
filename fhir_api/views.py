b0b5be4143f350deffcef8883721af48f58e99c2
Jakob Mathiszig-Lee <jakob@mathisziglee.co.uk>
2019-03-17 21:23:02 +0000

    some more progress, bul_update() not working

--- fhir_api/views.py
+++ fhir_api/views.py
@@ -3,6 +3,7 @@ Views for the fhir_api Opal Plugin
 """
 import pprint
 from django.views.generic import TemplateView
+from opal.models import Patient
 
 from fhirclient import client
 import fhirclient.models.patient as p
@@ -11,54 +12,52 @@ import fhirclient.models.medicationrequest as meds
 import fhirclient.models.medication as mednames
 import fhirclient.models as fhirmodels
 
+
 class FhirViewSet(TemplateView):
 
     template_name = "modals/test_fhir.html"
 
-    def get_context_data(request):
-        settings = {
-            'app_id': 'my_web_app',
-            'api_base': 'https://r3.smarthealthit.org'
-        }
+    def get_context_data(request, patient_id, episode_id, hospital_number):
+
+        # pprint.pprint(request.request.user.username)
+        pprint.pprint(dir(request.request.user))
+
+        settings = {"app_id": "my_web_app", "api_base": "https://r3.smarthealthit.org"}
 
         smart = client.FHIRClient(settings=settings)
 
-        #patient = s.FHIRSearch(p.Patient, {'_id': 'smart-7777701'}).perform(smart.server)
-        # patient = p.Patient.where(struct={'_id': 'smart-77777701'}).perform_resources(smart.server)
-        patient = s.FHIRSearch(p.Patient, {'_id': 'smart-767980'}).perform(smart.server)
+        patient = s.FHIRSearch(p.Patient, {"_id": "smart-767980"}).perform(smart.server)
         json_pt = patient.as_json()
 
-        pprint.pprint(json_pt["entry"][0]["resource"])
+        # pprint.pprint(json_pt["entry"][0]["resource"])
         patient = json_pt["entry"][0]["resource"]
         patient_object = {
             "birthdate": patient["birthDate"],
             "first_name": patient["name"][0]["given"][0],
             "surname": patient["name"][0]["family"],
         }
-        pprint.pprint(patient_object)
-        # try:
-        #     patient = s.FHIRSearch(p.Patient, {'_id': 'smart-77777701'}).perform_resource(smart.server)
-        #     json_pt = patient.as_json()
-        #     context = {
-        #         "patient": json_pt["entry"]
-        #     }
-        # except:
-        #     context = {
-        #         "patient": "No Patient found"
-        #     }
 
-        medications = meds.MedicationRequest.where(struct={'subject': 'smart-767980', 'status': 'active'}).perform_resources(smart.server)
+        medications = meds.MedicationRequest.where(
+            struct={"subject": "smart-767980", "status": "active"}
+        ).perform_resources(smart.server)
         medlist = []
         for medication in medications:
             medref = medication.as_json()
+            # pprint.pprint(medref)
             med_object = {
-                "name": medref["text"]["div"],
-                "dosage": medref["dosageInstruction"][0]["text"]
+                "name": medref["medicationCodeableConcept"]["text"],
+                "dosage": medref["dosageInstruction"][0]["text"],
             }
             medlist.append(med_object)
 
-        context = {
-            "patient": patient_object,
-            "medication": medlist
-        }
+        context = {"patient": patient_object, "medication": medlist}
+        Patient().bulk_update(
+            {
+                "demographics": {"hospital_number": hospital_number},
+                "drug_history": {"Medications": medlist},
+            },
+            request.request.user.username,
+            episode=episode_id,
+        )
+
         return context
