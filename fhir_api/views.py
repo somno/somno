8c9d2c96ebe031d84b03373dfdd56096c6297c8b
Jakob Mathiszig-Lee <jakob@mathisziglee.co.uk>
2019-03-19 13:17:44 +0000

    fixing

--- fhir_api/views.py
+++ fhir_api/views.py
@@ -3,7 +3,7 @@ Views for the fhir_api Opal Plugin
 """
 import pprint
 from django.views.generic import TemplateView
-from opal.models import Patient
+from opal.models import Patient, UserProfile
 
 from fhirclient import client
 import fhirclient.models.patient as p
@@ -17,11 +17,10 @@ class FhirViewSet(TemplateView):
 
     template_name = "modals/test_fhir.html"
 
-    def get_context_data(request, patient_id, episode_id, hospital_number):
-
-        # pprint.pprint(request.request.user.username)
-        pprint.pprint(dir(request.request.user))
-
+    def get_context_data(self, patient_id, episode_id, hospital_number):
+        #pprint.pprint(dir(request.request.user))
+        # user_profile = UserProfile.objects.get(pk=request.request.user.pk)
+        # pprint.pprint(dir(user_profile))
         settings = {"app_id": "my_web_app", "api_base": "https://r3.smarthealthit.org"}
 
         smart = client.FHIRClient(settings=settings)
@@ -53,10 +52,11 @@ class FhirViewSet(TemplateView):
         context = {"patient": patient_object, "medication": medlist}
         Patient().bulk_update(
             {
-                "demographics": {"hospital_number": hospital_number},
-                "drug_history": {"Medications": medlist},
+                "demographics": [{"hospital_number": hospital_number}],
+                "drug_history": [{"Medications": medlist}],
             },
-            request.request.user.username,
+            #request.request.user.username,
+            self.request.user,
             episode=episode_id,
         )
 
